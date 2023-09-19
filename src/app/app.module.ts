9923006d78d82f7537e22b1b5925d9fdd9e1e5a9
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-09-19 09:22:42 +0200

    Create components crear,detalla y lista eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEventosComponent } from './components/lista-eventos/lista-eventos.component';
+import { DetalleEventoComponent } from './components/detalle-evento/detalle-evento.component';
+import { CrearEventoComponent } from './components/crear-evento/crear-evento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEventosComponent,
+    DetalleEventoComponent,
+    CrearEventoComponent
   ],
   imports: [
     BrowserModule,
